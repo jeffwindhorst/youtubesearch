583f18917f253ef04120f8cdf27e71bb9bf39da5
Jeff Windhorst <jeffwindhorst@gmail.com>
2018-08-11 16:47:24 -0400

    Side links added through the API call with thumbs.

--- js/youtubePlayer.js
+++ js/youtubePlayer.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     // Sample Url
     // https://www.googleapis.com/youtube/v3/search?part=snippet&q=z6hQqgvGI4Y&type=video&videoDefinition=high&key=AIzaSyACVGRogzrd_OCytWCZf5fwHKO28ReWaTM
     
-    vidManager = {
-        baseUrl:"https://www.googleapis.com/youtube/v3/search?part=snippet",
-        apiKey:"AIzaSyACVGRogzrd_OCytWCZf5fwHKO28ReWaTM",
-        initVid:"z6hQqgvGI4Y",
-
+    var vidManager = {
+        baseUrl: "https://www.googleapis.com/youtube/v3/search?part=snippet",
+        apiKey: "AIzaSyACVGRogzrd_OCytWCZf5fwHKO28ReWaTM",
+        vidData: [],
+        
         vids: [
             'cjmbJmbIMtY',
             '0uBOtQOO70Y',
@@ -14,14 +14,24 @@ $(document).ready(function(){
             'OK_JCtrrv-c',
             'TlB_eWDSMt4',
             '9gTw2EDkaDQ',
-            'XYDbq5YfoqI',
+            'XYDbq5YfoqI'
         ],
-        
-        url:baseUrl+"&"+"key="+apiKey+"&q="+q,
         
         init: function() {
             this.sideLinksBind();
-            this.getVideo(this.initVid);
+            console.log('LENGTH ' + this.vidData.length);
+            for(i=0; i<this.vids.length; i++) {
+                console.log(this.vids[i]);
+                this.getVideo(this.vids[i]);
+            }
+        },
+        
+        buildSideLinks() {
+            {
+                this.vids.forEach(function(el) {
+                    // $('#home-sidelinks').append(<li><a class="smoothscroll" href="#about">About<span>who we are</span></a></li>')
+                });
+            };
         },
         
         sideLinksBind: function() {
@@ -32,12 +42,18 @@ $(document).ready(function(){
         
         
         getVideo: function(id) {   
-            console.log('URL: ' + thisurl);
+            
+            url=this.baseUrl+"&"+"key="+this.apiKey+"&q="+id
+            console.log('URL: ' + url);
             $.get(url, function(data){
-                vid=data.items[0].id.videoId;
-                vtitle=data.items[0].snippet.title;
-                vdesc=data.items[0].snippet.description;
-                vthumb=data.items[0].snippet.thumbnails.default;
+                vid = data.items[0].id.videoId;
+                vtitle = data.items[0].snippet.title;
+                vdesc = data.items[0].snippet.description;
+                vthumb = data.items[0].snippet.thumbnails.default.url;
+                vwidth = data.items[0].snippet.thumbnails.default.width;
+                vheight = data.items[0].snippet.thumbnails.default.height;
+                
+                $('.home-sidelinks').append("<li><img src='"+vthumb+"' width='"+vwidth+"' height='"+vheight+"'><a class='smoothscroll' href='" + vid + "'>" + vtitle + "</a></li>");
             });
         }, 
         
@@ -45,4 +61,6 @@ $(document).ready(function(){
             $('#vid')
         }
     };
+    vidManager.init();
 });
+
